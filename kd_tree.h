4c3a2dee333b5a243bec00f8a154f8726287f3fc
Tomer Davidor <tomer.davidor@gmail.com>
2018-01-05 06:57:31 -0800

    fix compilation errors

--- kd_tree.h
+++ kd_tree.h
@@ -1,102 +1,111 @@
 #ifndef KNN_KD_TREE
 #define KNN_KD_TREE
 
-#include "defs.h"
 #include <memory>
 #include <iterator>
 #include <algorithm>
-template <typename Kernel>
-class KDTree
-{
-  public:
+#include <iostream>
+
+template<typename Kernel>
+class KDTree {
+public:
     typedef typename Kernel::Point_d Point_d;
-    typedef typename Kernel::FT NumberType;
+    typedef typename Kernel::FT FT;
 
     // input: d - the dimension
     // iterator to the input points
     // axis - the axis to split by
-    template <typename InputIterator>
+    template<typename InputIterator>
     KDTree(size_t d, InputIterator beginPoints, InputIterator endPoints, size_t axis = 0);
 
     // input: const reference to a d dimensional vector which represent a d-point.
     // output: a vector of the indexes of the k-nearest-neighbors points
-    template <typename OutputIterator>
+    template<typename OutputIterator>
     OutputIterator find_points(size_t k, const Point_d &it, OutputIterator oi);
-    
+
     bool is_leaf() const;
-    void print(std::ostream& out = std::cout, size_t indent = 0) const;
-  private:
+
+    void print(std::ostream &out = std::cout, size_t indent = 0) const;
+
+private:
+    template<typename InputIterator>
+    InputIterator find_median_point(InputIterator &begin, InputIterator &end, size_t length) const;
+
     std::unique_ptr<KDTree<Kernel>> m_left;
     std::unique_ptr<KDTree<Kernel>> m_right;
     size_t m_dimension;
     size_t m_axis;
-    NumberType m_split_value;
+    FT m_split_value;
     Point_d m_point;
 };
 
-template <typename Kernel, typename InputIterator>
-KDTree<Kernel>::KDTree(size_t d, InputIterator begin, InputIterator end, size_t axis = 0)
-: m_left(nullptr), m_right(nullptr), m_dimension(d), m_axis(axis){
+template<typename Kernel>
+template<typename InputIterator>
+KDTree<Kernel>::KDTree(size_t d, InputIterator begin, InputIterator end, size_t axis/* = 0*/)
+        : m_left(nullptr), m_right(nullptr), m_dimension(d), m_axis(axis) {
     size_t points_size = std::distance(begin, end);
+    // points_size < 0 ?!
     if (points_size == 1) {
         // leaf!
         m_point = *begin;
     } else {
         // mid point - need to pivot
         auto pivot = find_median_point(begin, end, points_size);
-        m_split_value = *pivot[m_axis];
+        m_split_value = (*pivot)[m_axis];
         // go deeper
         auto next_axis = (m_axis + 1) % m_dimension;
         if (pivot != begin) {
-            m_left = std::make_unique<KDTree<Kernel>>(d, begin, pivot, next_axis);
+            m_left.reset(new KDTree<Kernel>(d, begin, pivot, next_axis));
         }
         if (pivot != end) {
-            m_right = std::make_unique<KDTree<Kernel>>(d, pivot, end, next_axis);
+            m_right.reset(new KDTree<Kernel>(d, pivot, end, next_axis));
         }
 
     }
 }
 
-template <typename Kernel, typename InputIterator>
-InputIterator KDTree<Kernel>::find_median_point(InputIterator& begin, InputIterator& end, size_t length) {
+template<typename Kernel>
+template<typename InputIterator>
+InputIterator KDTree<Kernel>::find_median_point(InputIterator &begin, InputIterator &end, size_t length) const {
     auto median = begin + length / 2;
-    std::nth_element(begin, median, end, [&](Point_d p1, Point_d p2) {return p1[m_axis] < p2[m_axis];});
+    std::nth_element(begin, median, end, [&](Point_d p1, Point_d p2) { return p1[m_axis] < p2[m_axis]; });
     auto median_axis_value = (*median)[m_axis];
-    auto split = std::partition(begin, end, [&median_axis_value](auto p) {return p[m_axis] <= median_axis_value;});
+    auto split = std::partition(begin, end, [&](Point_d p) { return p[m_axis] <= median_axis_value; });
     // TODO: handle degenerate cases
     return split;
 
 }
 
-template <typename Kernel, typename OutputIterator>
+template<typename Kernel>
+template<typename OutputIterator>
 OutputIterator KDTree<Kernel>::find_points(size_t k, const Point_d &it, OutputIterator oi) {
     return oi;
 }
 
 
-
-template <typename Kernel>
+template<typename Kernel>
 bool KDTree<Kernel>::is_leaf() const {
     return m_left == nullptr && m_right == nullptr;
 }
 
 // helper function - prints the tree in a json format for debugging
-template <typename Kernel>
-void KDTree<Kernel>::print(std::ostream& out = std::cout, size_t indent = 0) const{
+template<typename Kernel>
+void KDTree<Kernel>::print(std::ostream &out/* = std::cout*/, size_t indent/* = 0*/) const {
     auto space = std::string(indent, ' ');
     out << space << "{" << std::endl;
     if (is_leaf()) {
         out << space << "leaf: " << m_point << std::endl;
     } else {
-        cout << space << "split node: { axis: " << m_axis << ", value: " << m_split_value << "}"<< std::endl;
-        cout << space << "left:" << std::endl;
+        out << space << "split node: { axis: " << m_axis << ", value: " << m_split_value << "}" << std::endl;
+        out << space << "left:" << std::endl;
         m_left->print(out, indent + 2);
-        cout << space << "right:" << std::endl;
+        out << space << "right:" << std::endl;
         m_right->print(out, indent + 2);
     }
     out << space << "}" << std::endl;
 
 }
+
 /*
 template<typename InputIterator>
 KDTree::KDTree(size_t d, InputIterator begin_points, InputIterator end_points)
--- knn.h
+++ knn.h
@@ -7,30 +7,31 @@
 
 #include <vector>
 #include <memory>
+#include "kd_tree.h"
 
-template <typename Kernel>
+template<typename Kernel>
 class Knn {
 public:
     typedef typename Kernel::Point_d Point_d;
+
     // input: d - the dimension
     // iterator to the input points
-    template <typename InputIterator>
-            Knn(size_t d, InputIterator beginPoints, InputIterator endPoints)
-    {
-        m_root = std::make_unique(new KDTree<Kernel>(d, beginPoints, endPoints));
+    template<typename InputIterator>
+    Knn(size_t d, InputIterator beginPoints, InputIterator endPoints)
+        :m_root(new KDTree<Kernel>(d, beginPoints, endPoints)){
         m_root->print();
     }
 
     // input: const reference to a d dimensional vector which represent a d-point.
     // output: a vector of the indexes of the k-nearest-neighbors points
-    template <typename OutputIterator>
-    OutputIterator find_points(size_t k,const Point_d &it,OutputIterator oi)
-    {
+    template<typename OutputIterator>
+    OutputIterator find_points(size_t k, const Point_d &it, OutputIterator oi) {
         //your query code
         return oi;
     }
+
 private:
-std::unique_ptr<KDTree<Kernel>> m_root {nullptr};
+    std::unique_ptr<KDTree<Kernel>> m_root{nullptr};
 };
 
 #endif //KD_TREE_KNN_KNN_H
